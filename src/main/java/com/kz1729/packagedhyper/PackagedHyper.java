091b3c1a6524c541588b532cea659cf5638fbd48
tachibana-kazuma <kazumaty2008@gmail.com>
2026-01-05 17:27:24 +0900

    [start]Hyperlinkとpackaged autoのmod開発を 開始 最小構成で起動

---
+++ src/main/java/com/kz1729/packagedhyper/PackagedHyper.java
@@ -0,0 +1,10 @@
+package com.kz1729.packagedhyper;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("packagedhyper")
+public class PackagedHyper {
+    public PackagedHyper() {
+        // まだ何も書かなくてOK
+    }
+}
